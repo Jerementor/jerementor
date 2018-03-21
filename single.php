cf03ae7e1436264507b798ebb62c56f6e88cb3be
Jeremy Alexander <jwalexander22@gmail.com>
2018-03-21 21:52:47 +0000

    categories working

--- single.php
+++ single.php
@@ -14,38 +14,39 @@ get_header();
 <div class="w-container">
 <div class="jer_640">
     <h1 class="jer_header"><?php wp_title(''); ?></h1>
-    <h3><?php the_excerpt(); ?></h3>
     <div class="jer_blog_meta">
+		
 		<?php
-			$categories = get_categories( array(
+		$categories = get_categories( array(
 			    'orderby' => 'name',
 			    'order'   => 'ASC'
 			) );
-	 
+			 
 			foreach( $categories as $category ) {
 			    $category_link = sprintf( 
-			        '<a href="%1$s" alt="%2$s">%3$s" class="jer_category_on_post" ></a>',
+			        '<a class="jer_category_on_post" href="%1$s"  alt="%2$s">%3$s</a>',
 			        esc_url( get_category_link( $category->term_id ) ),
 			        esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ),
 			        esc_html( $category->name )
 			    );
 			     
-			 echo '<p>' . sprintf(__('%s', 'textdomain' ), $category_link ) . '</p> ';
+			    echo '' . sprintf( esc_html__( '%s', 'textdomain' ), $category_link ) . ' ';
 			    // echo '<p>' . sprintf( esc_html__( 'Description: %s', 'textdomain' ), $category->description ) . '</p>';
 			    // echo '<p>' . sprintf( esc_html__( 'Post Count: %s', 'textdomain' ), $category->count ) . '</p>';
-			}
+			} 
 		?>
     </div>
-<?php 
-	if ( have_posts() ) {
-		while ( have_posts() ) {
-			the_post(); 
-			//
-			the_content();
-			//
-		} // end while
-	} // end if
-?>
+  
+	<?php 
+		if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post(); 
+				//
+				the_content();
+				//
+			} // end while
+		} // end if
+	?>
 
 </div>
 </div>
