c0e21112dd78e7a0ebf251058b975d6249ffd315
Jeremy Alexander <jwalexander22@gmail.com>
2018-03-21 21:25:35 +0000

    categories2.0

--- single.php
+++ single.php
@@ -24,13 +24,13 @@ get_header();
 	 
 			foreach( $categories as $category ) {
 			    $category_link = sprintf( 
-			        '<a href="%1$s" alt="%2$s">%3$s class="jer_category_on_post" ></a>',
+			        '<a href="%1$s" alt="%2$s">%3$s" class="jer_category_on_post" ></a>',
 			        esc_url( get_category_link( $category->term_id ) ),
 			        esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ),
 			        esc_html( $category->name )
 			    );
 			     
-			 echo '<a>' . sprintf(__('%s', 'textdomain' ), $category_link ) . '</a> ';
+			 echo '<p>' . sprintf(__('%s', 'textdomain' ), $category_link ) . '</p> ';
 			    // echo '<p>' . sprintf( esc_html__( 'Description: %s', 'textdomain' ), $category->description ) . '</p>';
 			    // echo '<p>' . sprintf( esc_html__( 'Post Count: %s', 'textdomain' ), $category->count ) . '</p>';
 			}
