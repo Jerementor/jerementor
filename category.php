87ceecec82a24adf4b3d4b7e526abf6adbb0cfd0
Jeremy Alexander <jwalexander22@gmail.com>
2018-03-22 01:10:52 +0000

    added all category functionality, bug: with categories on single.php

--- category.php
+++ category.php
@@ -1,31 +1,28 @@
-<?php 
+<?php
 /**
- * The template for the index page
+ * The template for displaying Category pages
  *
- * 
- * @since 1.0
+ * @link https://codex.wordpress.org/Template_Hierarchy
  */
 get_header(); 
 ?>
 
-<!-- Index.php -->
+<!-- Category.php -->
 <div class="section-140">
 	<div class="w-container">
-    <h1 class="jer_header">All Articles</h1>
-    <p>Every article written so far with new ones constantly being added</p>
-    
+    <h1 class="jer_header"><?php single_cat_title(); ?></h1>
     <div class="w-row">
         <div class="w-col w-col-7">
           	<ul class="jer_blog_ul">
 		     <?php if ( have_posts() ) : ?>
         		<?php while ( have_posts() ) : the_post(); ?>
-        			<li>
-           				<?php the_title(sprintf('<a class="jer_link" href="%s" rel="bookmark">', esc_url(get_permalink() )), '</a>'); ?>
-					</li>
+        		    <li>
+           			    <?php the_title(sprintf('<a class="jer_link" href="%s" rel="bookmark">', esc_url(get_permalink() )), '</li>'); ?>
+                    </li>
 		        <?php endwhile; ?>
-		    <?php endif; ?> 
-		    
-          	</ul>
+		    <?php endif; ?>    		
+          	</ul>            
+
         </div>
         
         <div class="w-col w-col-5">
@@ -36,4 +33,5 @@ get_header();
 
 	</div>
 </div>
+
 <?php get_footer(); ?>
