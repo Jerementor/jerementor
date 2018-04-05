3fbaab6fd5a600d56ce308cfa181d4bd67539a59
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-05 22:54:57 +0000

    main-section updated

--- index.php
+++ index.php
@@ -11,24 +11,41 @@ get_header();
 <!-- Index.php -->
 <div class="section-140">
 	<div class="w-container">
-    <h1 class="jer_header">All Articles</h1>
-    
-    <div class="w-row">
-        <div class="w-col w-col-7">
-          	<ul class="jer_blog_ul">
-		     <?php if ( have_posts() ) : ?>
-        		<?php while ( have_posts() ) : the_post(); ?>
-        			<li>
-           				<?php the_title(sprintf('<a class="jer_link" href="%s" rel="bookmark">', esc_url(get_permalink() )), '</a>'); ?>
-					</li>
-		        <?php endwhile; ?>
-		    <?php endif; ?> 
+      <div class="w-row">
+        <div class="w-col w-col-8">
+          <div class="w-row">
+            <div class="column w-col w-col-3"><img src="/wp-content/uploads/2018/03/jer3.jpg" width="105" class="image"></div>
+            <div class="w-col w-col-9">
+              <h1 class="jer_min_header">Jeremy Alexander</h1>
+              <p class="paragraph-2">Thoughts on e-commerce, specifically courses, ebooks, memberships, SaaS, design and marketing.</p>
+            </div>
+          </div>
+          <ul class="jer_blog_ul">
+             <?php if ( have_posts() ) : ?>
+                    		<?php while ( have_posts() ) : the_post(); ?>
+                    			<li>
+                       				<?php the_title(sprintf('<a class="jer_link" href="%s" rel="bookmark">', esc_url(get_permalink() )), '</a>'); ?>
+            					</li>
+            		        <?php endwhile; ?>
+            <?php endif; ?> 
+		    
+          </ul>
+        </div>
+        <div class="w-col w-col-4">
+        <?php get_sidebar(); ?>	            
+          <!--<div class="jer_categories">-->
+          <!--  <h3 class="jer_min_header">Categories</h3>-->
+          <!--  <ul class="unordered-list">-->
+              
+          <!--  </ul>-->
+          <!--</div>-->
+        </div>
 		    
           	</ul>
         </div>
         
         <div class="w-col w-col-5">
-	        <?php get_sidebar(); ?>	
+	
         </div>
     </div>
 
--- page-main-section.php
+++ page-main-section.php
@@ -9,17 +9,37 @@ get_header();
 ?>
 <!-- Main Section -->
 <div class="section-140">
+	<div class="w-container">
+      <div class="w-row">
+        <div class="w-col w-col-8">
+          <div class="w-row">
+            <div class="column w-col w-col-3"><img src="/wp-content/uploads/2018/03/jer3.jpg" width="105" class="image"></div>
+            <div class="w-col w-col-9">
+              <h1 class="jer_min_header">Jeremy Alexander</h1>
+              <p class="paragraph-2">Thoughts on e-commerce, specifically courses, ebooks, memberships, SaaS, design and marketing.</p>
+            </div>
+          </div>
+          <ul class="jer_blog_ul">
+             <?php if ( have_posts() ) : ?>
+                    		<?php while ( have_posts() ) : the_post(); ?>
+                    			<li>
+                       				<?php the_title(sprintf('<a class="jer_link" href="%s" rel="bookmark">', esc_url(get_permalink() )), '</a>'); ?>
+            					</li>
+            		        <?php endwhile; ?>
+            <?php endif; ?> 
+		    
+          </ul>
+        </div>
+        <div class="w-col w-col-4">
+        	<?php get_sidebar(); ?>	            
+    
+        </div>
 
-		<?php 
-		if ( have_posts() ) {
-			while ( have_posts() ) {
-				the_post(); 
-				//
-				the_content();
-				//
-			} // end while
-		} // end if
-		?>
-</div>	
-
+        </div>
+        
+        <div class="w-col w-col-5">
+	
+        </div>
+    </div>
+	</div>
 <?php get_footer();
