468e5cb70c566b840611422c1effc541b105a803
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-06 21:00:17 +0000

    remove crash!

--- page-main-section.php
+++ page-main-section.php
@@ -19,16 +19,7 @@ get_header();
   </div>
   <div class="w-row">
     <div class="w-col w-col-7">
-		<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-		$args = array( 'post_type' => 'post', 'posts_per_page' => 10, 'paged' => $paged );
-		$wp_query = new WP_Query($args);
-		while ( have_posts() ) : the_post(); ?>
-		    <h2><?php the_title() ?></h2>
-		<?php endwhile; ?>
-		
-		<!-- then the pagination links -->
-		<?php next_posts_link( '&larr; Older posts', $wp_query ->max_num_pages); ?>
-		<?php previous_posts_link( 'Newer posts &rarr;' ); ?>    	
+	
     	
      <!-- <ul class="jer_blog_ul"> -->
 		<!--<!?php if(have_posts()) : ?>-->
