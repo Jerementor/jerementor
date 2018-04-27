8843f488451b809be68c969cae41947f40a3fdf9
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-27 20:16:57 +0000

    added new nav

--- template-parts/nav/logged_out_nav.php
+++ template-parts/nav/logged_out_nav.php
@@ -1,6 +1,16 @@
 <div class="jer_nav">
-     <a href="/">
-        <h3 class="jer_brand_link">jerementor</h3>
-      </a>  
-  <?php get_template_part( 'template-parts/nav/nav_out'); ?>
+<div data-collapse="medium" data-animation="default" data-duration="400" class="w-nav">
+<div class="container w-container">
+  <a href="#" class="w-nav-brand">
+    <h2 class="jer_brand_link">jerementor</h2>
+  </a>
+  <nav role="navigation" class="w-nav-menu">
+    <?php get_template_part( 'template-parts/nav/nav_out'); ?>
+  </nav>
+  <div class="w-nav-button">
+    <div class="w-icon-nav-menu"></div>
+  </div>
+</div>
+</div>
+
 </div>
