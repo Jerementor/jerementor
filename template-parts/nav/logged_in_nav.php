9d687ceae6d5d4951bab009be86acba578138b57
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-27 20:08:44 +0000

    navbar

--- template-parts/nav/logged_in_nav.php
+++ template-parts/nav/logged_in_nav.php
@@ -1,4 +1,6 @@
 <div class="jer_nav">
-     <a href="/" class="jer_brand_link">jerementor</a>  
+     <a href="/">
+        <h3 class="jer_brand_link">jerementor</h3>
+      </a>     
     <?php get_template_part( 'template-parts/nav/nav_out'); ?>
 </div>
