1693b21df1fddd1a1c55f446ae03431c6011a07c
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-27 20:37:32 +0000

    navbar 3

--- template-parts/nav/logged_out_nav.php
+++ template-parts/nav/logged_out_nav.php
@@ -1,16 +1,13 @@
-<div class="jer_nav">
-<div data-collapse="medium" data-animation="default" data-duration="400" class="w-nav">
-<div class="container w-container">
-  <a href="/" class="w-nav-brand">
-    <h2 class="jer_brand_link">jerementor</h2>
-  </a>
-  <nav role="navigation" class="w-nav-menu">
-    <?php get_template_part( 'template-parts/nav/nav_out'); ?>
-  </nav>
-  <div class="w-nav-button">
-    <div class="w-icon-nav-menu"></div>
+<div data-collapse="medium" data-animation="default" data-duration="400" class="jer_navbar w-nav">
+  <div class="w-container">
+    <a href="/" class="w-nav-brand">
+      <h2 class="jer_brand">jerementor</h2>
+    </a>
+    <nav role="navigation" class="w-nav-menu">
+    <?php get_template_part( 'template-parts/nav/nav_out'); ?>    
+    </nav>
+    <div class="menu-button w-nav-button">
+      <div class="w-icon-nav-menu"></div>
+    </div>
   </div>
-</div>
-</div>
-
 </div>
