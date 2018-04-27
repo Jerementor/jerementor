b75d39b678e6f29a8bd6fb5781e26a4e490d5f85
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-27 19:49:19 +0000

    single.php

--- single.php
+++ single.php
@@ -10,28 +10,23 @@ get_header();
 ?>
 
 <!--SINGLE.PHP-->
-<div class="section-80">
-<div class="w-container">
-<div class="jer_640">
-    <h1 class="jer_header"><?php wp_title(''); ?></h1>
-    <?php the_excerpt(); ?>
-    <div class="jer_blog_meta">
-		<!--<!?php get_template_part('template-parts/get_category'); ?>-->
-		<?php the_category(' ', 'multiple')?>		
-    </div>
-  
-	<?php 
-		if ( have_posts() ) {
-			while ( have_posts() ) {
-				the_post(); 
-				//
-				the_content();
-				//
-			} // end while
-		} // end if
-	?>
+<h1 class="jer_home_header"><?php wp_title(''); ?></h1>
+
+<div class="jer_meta_data">
+	<?php the_excerpt(); ?>
+	<?php the_category(' ', 'multiple')?>		
+</div>
+<div class="jer_blog">
+<?php 
+	if ( have_posts() ) {
+		while ( have_posts() ) {
+			the_post(); 
+			//
+			the_content();
+			//
+		} // end while
+	} // end if
+?>
+</div>
 <a href="/" class="jer_link_back">Back to home</a>
-</div>
-</div>
-</div>
 <?php get_footer(); ?>
