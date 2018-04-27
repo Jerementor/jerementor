fa17187b313219802542f3ec8edb6a1609924417
Jeremy Alexander <jwalexander22@gmail.com>
2018-04-27 20:29:40 +0000

    luck be a lady

--- template-parts/nav/logged_out_nav.php
+++ template-parts/nav/logged_out_nav.php
@@ -1,7 +1,7 @@
 <div class="jer_nav">
 <div data-collapse="medium" data-animation="default" data-duration="400" class="w-nav">
 <div class="container w-container">
-  <a href="#" class="w-nav-brand">
+  <a href="/" class="w-nav-brand">
     <h2 class="jer_brand_link">jerementor</h2>
   </a>
   <nav role="navigation" class="w-nav-menu">
